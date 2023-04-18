5f5a1db5f2c54def90cbd73774fc773b9bd1af53
VolLove <103765155+VolLove@users.noreply.github.com>
2023-04-18 19:46:20 +0700

    update 18/4/2023

---
+++ example-app/resources/views/FontEnd/register.blade.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <!-- Meta Tag -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name='copyright' content=''>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Title Tag  -->
+    <title>Eshop - Register.</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    <!-- Web Font -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
+        rel="stylesheet">
+
+    <!-- StyleSheet -->
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="css/magnific-popup.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <!-- Fancybox -->
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <!-- Themify Icons -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- Nice Select CSS -->
+    <link rel="stylesheet" href="css/niceselect.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Flex Slider CSS -->
+    <link rel="stylesheet" href="css/flex-slider.min.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="css/owl-carousel.css">
+    <!-- Slicknav -->
+    <link rel="stylesheet" href="css/slicknav.min.css">
+
+    <!-- Eshop StyleSheet -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+
+
+</head>
+
+<body class="js">
+
+
+    <!-- Header -->
+    <header class="header shop">
+        <!-- Topbar -->
+        <div class="topbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 col-12">
+                    </div>
+                    <div class="col-lg-8 col-md-12 col-12">
+                        <!-- Top Right -->
+                        <div class="right-content">
+                            <ul class="list-main">
+                                <li><i class="ti-user"></i> <a href="#">My account</a></li>
+                                <li><i class="ti-power-off"></i><a href="{{ url('login') }}">Login</a></li>
+                            </ul>
+                        </div>
+                        <!-- End Top Right -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Topbar -->
+
+
+        <div class="middle-inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-md-2 col-12">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="index.html"><img src="images/logo.png" alt="logo"></a>
+                        </div>
+                        <!--/ End Logo -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="header-inner">
+            <div class="container">
+                <div class="cat-nav-head ">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="menu-area">
+                                <!-- Main Menu -->
+                                <nav class="navbar navbar-expand-lg">
+                                    <div class="navbar-collapse">
+                                        <div class="nav-inner">
+                                            <ul class="nav main-menu menu navbar-nav">
+                                                <li class="active"><a href="#">Home</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </nav>
+                                <!--/ End Main Menu -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container my-5">
+            <div class="mb-5">
+                <h1>Register</h1>
+            </div>
+            <form class="form" method="post " action="#">
+                <div class="row mb-3">
+                    <div class="col-2"> <label>User name</label></div>
+                    <div class="col-10"> <input style="width: 400px" type="text" name="username" id="username"
+                            placeholder="User name" required="required"></div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-2"> <label>Password</label></div>
+                    <div class="col-10"> <input style="width: 400px" type="password" name="password" id="password"
+                            placeholder="passowrd" required="required"></div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-2"> <label>Repear Password</label></div>
+                    <div class="col-10"> <input style="width: 400px" type="password" name="repassword"
+                            id="repassword" placeholder="Repeat password" required="required"></div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-2"> <label>Your phone</label></div>
+                    <div class="col-10"> <input style="width: 400px" type="text" name="numberphone"
+                            id="numberphone" placeholder="Your phone" required="required"></div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-2"> <label>Avata image</label></div>
+                    <div class="col-10"> <input style="width: 400px" accept="image/*" type="file"
+                            name="imageface" id="imageface" required="required"></div>
+                </div>
+                <div class="">
+                    <button type="submit" class="btn btn-primary mb-3">Login</button>
+                </div>
+            </form>
+        </div>
+        </form>
+        </div>
+
+        <!-- Start Footer Area -->
+        <footer class="footer">
+            <!-- Footer Top -->
+            <div class="footer-top section">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-3 col-md-6 col-12">
+                            <!-- Single Widget -->
+                            <div class="single-footer social">
+                                <h4>Get In Tuch</h4>
+                                <!-- Single Widget -->
+                                <div class="contact">
+                                    <ul>
+                                        <li>NO. 342 - London Oxford Street.</li>
+                                        <li>012 United Kingdom.</li>
+                                        <li>info@eshop.com</li>
+                                        <li>+032 3456 7890</li>
+                                    </ul>
+                                </div>
+                                <!-- End Single Widget -->
+                                <ul>
+                                    <li><a href="#"><i class="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i class="ti-twitter"></i></a></li>
+                                    <li><a href="#"><i class="ti-flickr"></i></a></li>
+                                    <li><a href="#"><i class="ti-instagram"></i></a></li>
+                                </ul>
+                            </div>
+                            <!-- End Single Widget -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Footer Top -->
+            <div class="copyright">
+                <div class="container">
+                    <div class="inner">
+                        <div class="row">
+                            <div class="col-lg-6 col-12">
+                                <div class="left">
+                                    <p>Copyright © 2020 <a href="http://www.wpthemesgrid.com">Wpthemesgrid</a> - All
+                                        Rights
+                                        Reserved.</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-12">
+                                <div class="right">
+                                    <img src="images/payments.png" alt="#">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- /End Footer Area -->
+
+        <!-- Jquery -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery-migrate-3.0.0.js"></script>
+        <script src="js/jquery-ui.min.js"></script>
+        <!-- Popper JS -->
+        <script src="js/popper.min.js"></script>
+        <!-- Bootstrap JS -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Color JS -->
+        <script src="js/colors.js"></script>
+        <!-- Slicknav JS -->
+        <script src="js/slicknav.min.js"></script>
+        <!-- Owl Carousel JS -->
+        <script src="js/owl-carousel.js"></script>
+        <!-- Magnific Popup JS -->
+        <script src="js/magnific-popup.js"></script>
+        <!-- Fancybox JS -->
+        <script src="js/facnybox.min.js"></script>
+        <!-- Waypoints JS -->
+        <script src="js/waypoints.min.js"></script>
+        <!-- Countdown JS -->
+        <script src="js/finalcountdown.min.js"></script>
+        <!-- Nice Select JS -->
+        <script src="js/nicesellect.js"></script>
+        <!-- Ytplayer JS -->
+        <script src="js/ytplayer.min.js"></script>
+        <!-- Flex Slider JS -->
+        <script src="js/flex-slider.js"></script>
+        <!-- ScrollUp JS -->
+        <script src="js/scrollup.js"></script>
+        <!-- Onepage Nav JS -->
+        <script src="js/onepage-nav.min.js"></script>
+        <!-- Easing JS -->
+        <script src="js/easing.js"></script>
+        <!-- Active JS -->
+        <script src="js/active.js"></script>
+</body>
+
+</html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('FontEnd/index');
+Route::prefix('/')->group(function () {
+    Route::get('/', function () {
+        return view('FontEnd/index');
+    });
+    Route::get('/login', function () {
+        return view('FontEnd/login');
+    });
+    Route::get('/register', function () {
+        return view('FontEnd/register');
+    });
+
+    Route::get('/product', function () {
+        return view('FontEnd/product');
+    });
+
+    Route::get('/shopgrid', function () {
+        return view('FontEnd/shopgrid');
+    });
+
+    Route::get('/checkout', function () {
+        return view('FontEnd/checkout');
+    });
+
+    Route::get('/cart', function () {
+        return view('FontEnd/cart');
+    });
+});
+
+Route::prefix('Admind', function () {
+    return view('BackEnd/index');
 });
