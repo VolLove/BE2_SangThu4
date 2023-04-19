4d160daf9025edbfffb4698dedee55db6908ece4
VolLove <103765155+VolLove@users.noreply.github.com>
2023-04-19 21:15:19 +0700

    up

--- example-app/app/Http/Controllers/CustomAuthController.php
+++ example-app/app/Http/Controllers/CustomAuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
 
 class CustomAuthController extends Controller
 {
@@ -37,13 +40,45 @@ class CustomAuthController extends Controller
 
         return redirect("login")->withSuccess('Login details are not valid');
     }
+
+
     public function customRegistration(Request $request)
     {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'phone' => 'required',
+            'avata' => 'required|image|mimes:png,jpg,jpeg|max:2048',
+        ]);
+        $avatarName = time() . '.' . $request->avata->extension();
+        $request->image->move('uploads/avatars', $avatarName);
+
+        $data = $request->all();
+        $check = $this->create($data);
+
+        return redirect("dashboard")->withSuccess('You have signed-in');
     }
-    public function logout(Request $request)
+
+    public function create(array $data)
     {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'phone' => $data['phone'],
+            'avata' => $data['avata'],
+        ]);
     }
-    public function account(Request $request)
+
+
+    public function logout()
+    {
+        Session::flush();
+        Auth::logout();
+        return Redirect('login');
+    }
+    public function account()
     {
     }
 }
