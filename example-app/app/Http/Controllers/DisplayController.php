0a4785a03245b89e9d9b2a03d0f1a80198e64039
VolLove <103765155+VolLove@users.noreply.github.com>
2023-05-24 00:06:51 +0700

    update

--- example-app/app/Http/Controllers/DisplayController.php
+++ example-app/app/Http/Controllers/DisplayController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Products;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class DisplayController extends Controller
--- example-app/app/Http/Controllers/RegisterController.php
+++ example-app/app/Http/Controllers/RegisterController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Termwind\Components\Dd;
 
 class RegisterController extends Controller
 {
